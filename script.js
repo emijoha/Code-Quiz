1ad4850d9240d43a5ab2e3c9c5c9f57a123f229f
emi <ejhartline@gmail.com>
2020-03-07 18:20:24 -0500

    creating questions array, commented css and html files for readability

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-// Question objects array
-var questions = [
+// Questions as objects, stored in questions array
+var quizQuestions = [
     {
-        // question: "What is the answer to this question?",
-        // answers: ["1. Answer", "2. Answer", "3. Answer", "4.Answer"],
-        // correct: 
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: ["1. <js>", "2. <script>", "3. <javascript>", "4. <link>"],
+        correct: 1
+    },
+
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        answers: ["1. onmouseover", "2. onchange", "3. onmouseclick", "4. onclick"],
+        correct: 3
+    },
+
+    {
+        question: "Which do you use to display a message in an alert box?",
+        answers: ["1. msgBox()", "2. alertBox()", "3. msg()", "4. alert()"],
+        correct: 3
+    },
+
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answers: ["1. x", "2. *", "3. =", "4. -"],
+        correct: 2
     }
-]
+];
+
+//  Global variables
+var currentQuestion = 0;
+var secondsLeft = 75;
 
 // Grab elements
 var startButton = document.getElementById("startButton");
 var timeDisplay = document.getElementById("timer");
 
-// On clicking #startButton element, loads quiz.html page in same tab
+// On clicking #startButton element, loads quiz.html page in same tab. only works when on index.html page
 startButton.addEventListener("click", function() {
     window.location.replace("quiz.html");
 });
-
-// Create time variable, counting down from 75 seconds
-var time = 75;
-
-// Quiz countdown timer function
-function quizTimer() {
-    
-}
