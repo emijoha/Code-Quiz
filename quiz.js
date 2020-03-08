aeb60b98735771339fabe76dfa5cd005bad72449
emi <ejhartline@gmail.com>
2020-03-08 13:12:50 -0400

    RESOLVED: result message displays for 250 miliseconds and then reverts to blank

--- quiz.js
+++ quiz.js
@@ -37,8 +37,9 @@ var secondsLeft = 75;
 var timeDisplay = document.getElementById("timer");
 var quizDisplay = document.getElementById("question-box");
 var questionDisplay = document.getElementById("question-display");
-// Below, querySelectorAll creates array of the buttons in the order they are in html.
-var optionButtons = document.getElementById("button-wrapper");
+var optionButtonsDiv = document.getElementById("button-wrapper");
+// Below, querySelectorAll creates Node list array of the buttons in the order they are in html.
+var allOptions = document.querySelectorAll("button");
 var optionOne = document.getElementById("option-1");
 var optionTwo = document.getElementById("option-2");
 var optionThree = document.getElementById("option-3");
@@ -52,8 +53,6 @@ var resultDisplay = document.getElementById("result-display");
 var startTimer = setInterval(function() {
     if (secondsLeft <= 0) {
         clearInterval(startTimer);
-        // // store final secondsLeft locally, if questions finished before time, also all clearInterval and store
-        // localStorage.setItem("scoreTime", secondsLeft);
         // quizOver = true; 
     } 
     else {
@@ -63,21 +62,11 @@ var startTimer = setInterval(function() {
     
 }, 1000);
 
-// Testing that textContent will successfully change, and is targeted where desired
-// Eventually, this will help build the for loop, to go through each question after an option button click
-//____________________________________________________________________________________________
-questionDisplay.textContent = "Will this display a question?";
-optionOne.textContent = "1. This";
-optionTwo.textContent = "2. That";
-optionThree.textContent = "3. Here";
-optionFour.textContent = "4. There";
-resultDisplay.textContent = "";
-
 // Event Listener and validation (wrong or correct message display)
 // Eventually, rendering next questions will be worked into this function
 //____________________________________________________________________________________________
 // add the event listener to the div containing the option buttons
-optionButtons.addEventListener("click", (event) => {
+optionButtonsDiv.addEventListener("click", function(event) {
 
     // target isButton if its nodeName is BUTTON, so that clicking the surrounding div does not trigger event function
     var isButton = event.target.nodeName === "BUTTON";
@@ -88,18 +77,45 @@ optionButtons.addEventListener("click", (event) => {
     }
 
     // Testing Validation: the textContent of the button clicked should equal quizQuestions[i].options[answer] to display "correct", all else display "wrong"
-   
     else if (event.target.textContent == "1. This") {
-        resultDisplay.textContent = "Correct!"
+        displayResult("Correct!");
     }
 
+    // SOLVED: When button clicked does not match answer, 10 seconds successfully deducted from timerscore (secondsLeft)
     else if (event.target.textContent != "1. This") {
-        resultDisplay.textContent = "Wrong!"
+        displayResult("Wrong!");
+        // BUG: When secondsLeft <=0 , time stops counting down, but time display can keep changing by -10 with adddition button clicks, will probably be solved by ending quiz after secondsLeft < 0 or quizOver = true
         secondsLeft = secondsLeft - 10;
         timeDisplay.innerText = secondsLeft;
     }
     
 });
 
- // Next step: figure out how to display result message for 500 or 250 milisecs
-// Following step: include in click event function/code for rendering the next question
+// Function for displaying result message and timing it to resert to blank after 500 milisecs
+//____________________________________________________________________________________________
+function displayResult(message) {
+    resultDisplay.textContent = message;
+
+    setTimeout(function() {
+        resultDisplay.textContent = "";
+    }, 250);
+};
+
+
+// Following step: include in click event the function/code for rendering the next question
+
+
+// PAST PSEUDOCODE / PAST TESTING
+//____________________________________________________________________________________________
+//____________________________________________________________________________________________
+
+
+//// RESOLVED: Was able to achieve this using a for loop and the allOptions node list array instead of targeting individual buttons
+// Testing that textContent will successfully change, and is targeted where desired
+//____________________________________________________________________________________________
+questionDisplay.textContent = "Will this display a question?";
+optionOne.textContent = "1. This";
+optionTwo.textContent = "2. That";
+optionThree.textContent = "3. Here";
+optionFour.textContent = "4. There";
+resultDisplay.textContent = "";
