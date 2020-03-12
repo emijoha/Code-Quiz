f5806818cef68cde9bf7bcf45b75f3f66231b32a
emi <ejhartline@gmail.com>
2020-03-11 21:03:18 -0400

    added two missing buttons (go back and clear highscore

--- highscores.js
+++ highscores.js
@@ -4,7 +4,10 @@ var playerScore = localStorage.getItem("playerScore");
 
 // Grab HTML elements
 // ____________________________________________________________________________________________
+var playerScoreDiv = document.getElementById("player-score-div");
 var playerScoreDisplay = document.getElementById("player-score-span");
+var goBackButton = document.getElementById("go-back");
+var clearButton = document.getElementById("clear-score");
 
 // call renderHighscore
 // ____________________________________________________________________________________________
@@ -15,3 +18,14 @@ renderHighscore();
 function renderHighscore() {
     playerScoreDisplay.textContent = playerScore;
 };
+
+// Go Back button click event: goes back to index.html page
+goBackButton.addEventListener("click", function() {
+    window.location.replace("index.html");
+});
+
+// Clear Highscore button click event: removes local storage and clears display div
+clearButton.addEventListener("click", function() {
+    playerScoreDiv.classList.add("hidden");
+    localStorage.removeItem("playerScore");
+});
